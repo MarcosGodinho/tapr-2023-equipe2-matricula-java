5250ce9c63e1ee075762e1d604816363e39898e6
Marcos Godinho <104370082+MarcosGodinho@users.noreply.github.com>
2023-10-03 00:50:29 +0000

    aulao

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tapr-2023-equipe2-matricula-java
+
+## Autenticação no AZURE
+[DOC](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt)
+
+```
+az login -u marcos.godinho@univille.br
+az login --use-device-code
+
+az ad signed-in-user show
+```
---
+++ microservmatricula/src/main/java/br/edu/univille/microservmatricula/MicroservmatriculaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.microservmatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservmatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservmatriculaApplication.class, args);
+	}
+
+}
